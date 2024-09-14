e022d02e2c12562ced3b76af9da956b8d4992c57
EC2 Default User <ec2-user@ip-172-31-19-223.us-east-2.compute.internal>
2024-09-14 16:20:08 +0000

    Auth modified

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,17 +1,21 @@
-import { defineAuth } from '@aws-amplify/backend';
+/**import { defineAuth } from '@aws-amplify/backend';*/
 
 /**
  * Define and configure your auth resource
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
  */
+/**export const auth = defineAuth({
+  loginWith: {
+    email: true,
+  },
+});
+*/
+import { defineAuth } from "@aws-amplify/backend";
 export const auth = defineAuth({
   loginWith: {
     email: true,
   },
 });
-
-import { defineAuth } from "@aws-amplify/backend";
-
 export const auth = defineAuth({
   loginWith: {
     email: {
